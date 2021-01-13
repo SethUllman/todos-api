1cac447cf52b0b2746693941e49db1c8c89c7435
Seth Ullman <seth.ullman98@gmail.com>
2021-01-12 20:20:00 -0700

    add rails_helper config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,22 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+require 'database_cleaner'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+
+Shoulda::Matchers.configure do |config|
+  config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
 
 require File.expand_path('../config/environment', __dir__)
 
